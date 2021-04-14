959c0e98bf67d5150dfa2b2ca78dae3e44f12555
block41 <xy.zou@qq.com>
2021-04-14 20:22:32 +0800

    update

--- test/test.cpp
+++ test/test.cpp
@@ -1,9 +1,26 @@
 #include <iostream>
-#include <iomanip>
+#include <vector>
+#include <fstream>
+
 using namespace std;
+
 int main() {
-	std::cout 
-		<< std::setw(5) << 'a'<< std::setfill('*')<< std::setw(5)<<'b'
-		<< std::endl;
-	return 0;
+    ifstream inFile("binary.dat", ios::in | ios::binary);
+
+    if (!inFile) {
+        cout << "error" << endl;
+        return 0;
+    }
+
+    float fea[128];
+    int n = 0;
+    while (inFile.read((char*)&fea[0], 128 * sizeof(float))) {        //print dim1 of each img
+        n++;
+    }
+    inFile.close();
+    cout << n << endl;
+    for (int i = 0;i < 128;i++)
+        cout << fea[i] << " ";
+
+    return 0;
 }
