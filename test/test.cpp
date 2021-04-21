99d1f0e84cb21e2d2ea51f316e4a4a2f359f115b
block41 <xy.zou@qq.com>
2021-04-21 20:17:31 +0800

    update

--- test/test.cpp
+++ test/test.cpp
@@ -1,11 +1,27 @@
 #include <iostream>
-#include <array>
-#include <vector>
+
+#define Maxsize 1000
 
 int main() {
+	int n;
+	int List[Maxsize]{-1};
 
-	int a[10];
-	std::vector<int> b;
-	std::array<int, 10> c;
+	std::cin >> n;
+	 
+	for (int i = 1; i <= n; i++) {    //在数组中加序列
+		List[i * (i - 1) / 2] = 1;
+		for (int j = 1; j < i - 1; j++) {
+			List[i * (i - 1) / 2 + j] = List[i * (i - 1) / 2 + j - i + 1] + List[i * (i - 1) / 2 + j - i];
+		}
+		List[i * (i + 1) / 2 - 1] = 1;
+	}
 
+	for (int i = 1; i <=n; i++) {    //输出
+		for (int j = 0; j < i ; j++) {
+			std::cout << List[i * (i - 1) / 2 + j] << " ";
+		}
+		std::cout << "\n";
+	}
+
+	return 0;
 }
